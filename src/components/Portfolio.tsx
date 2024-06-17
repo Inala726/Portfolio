2188baa12512a76df77278547190d19967b139a6
Inala <inalajacob740@gmail.com>
2024-06-17 01:52:51 +0100

    Completed responsiveness

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -25,14 +25,14 @@ const Portfolio = () => {
         id="home"
         className="border w-[100%] h-[90vh] bg-[url('/src/assets/images/pexels-misael-garcia-832776-1707823.jpg')] bg-no-repeat bg-center bg-cover rounded-xl text-white"
       >
-        <div className="p-4 flex items-center justify-center gap-[100px] ">
+        <div className="p-4 flex items-center justify-center gap-[100px] max-md:gap-[30px] max-sm:justify-between max-sm:px-10 w-[100%]  ">
           <div className="">
             <a href="#home">
-              <FaSquarespace size={40} />
+              <FaSquarespace size={30} />
             </a>
           </div>
-          <div className="">
-            <ul className="flex text-xl gap-[30px]">
+          <div className="max-sm:hidden">
+            <ul className="flex text-xl  gap-[30px] ">
               <a href="#home">
                 <li>HOME</li>
               </a>
@@ -54,9 +54,9 @@ const Portfolio = () => {
             <TiThMenu size={25} className=""/>
           </div>
         </div>
-        <div className="text flex gap-[30px] flex-col items-center justify-center mt-32 ">
-          <p className="text-6xl">I'M JACOB INALA PRINCE</p>
-          <p className="text-3xl">Software Engineer/ Frontend Developer</p>
+        <div className="text flex gap-[30px] text-center flex-col items-center justify-center mt-32 ">
+          <p className="text-6xl font-bold max-sm:text-3xl">I'M JACOB INALA PRINCE</p>
+          <p className="text-3xl ">Software Engineer/ Frontend Developer</p>
         </div>
       </div>
 
@@ -64,7 +64,7 @@ const Portfolio = () => {
         <div className="flex items-center justify-center font-extrabold text-3xl">
           About me
         </div>
-        <div className="text-lg text-left p-10 leading-10">
+        <div className="text-lg text-left p-10 leading-10 w-[100%]">
           <p>
             My name is Inala. I am passionate about coding, finding joy in
             solving complex problems and creating innovative solutions. Every
@@ -85,7 +85,7 @@ const Portfolio = () => {
             real-world challenges.
           </p>
           <a href="">
-            <button className="border px-4 bg-slate-300 mt-7 rounded-xl">
+            <button className="border px-4 bg-slate-300 mt-7 rounded-xl hover:bg-slate-900 hover:text-white hover:transition hover:ease-in-out">
               Get resume
             </button>
           </a>
@@ -96,7 +96,7 @@ const Portfolio = () => {
         <div className="flex items-center justify-center font-extrabold text-3xl">
           My Skills
         </div>
-        <div className="grid grid-cols-5 items-center gap-10 p-10 ml-20 ">
+        <div className="grid grid-cols-5  items-center gap-10 p-10 ml-10 max-md:ml-0 max-sm:grid-cols-2 ">
           <div className=" items-center">
             <img src={html} className="w-[100px]" />
           </div>
@@ -133,14 +133,14 @@ const Portfolio = () => {
         <div className="flex items-center justify-center mt-6 mb-6 font-extrabold text-3xl">
           My Projects
         </div>
-        <div className="flex items-center justify-start p-10 gap-10">
-          <div className="w-[350px] h-[320px] border border-black p-1 rounded-lg">
+        <div className="flex items-center justify-start p-10 gap-10 max-sm:flex-col">
+          <div className="w-[350px] h-[fit-content] p-2 border border-black  rounded-lg max-sm:w-[95%]">
             <img src={picture} className="" />
             <p className="mt-[5px] font-bold">ICONS8 CLONE</p>
             <p>A well implemented clone of the icons8 website using HTML and CSS only</p>
             <a href="https://icons8-green.vercel.app/" target="_blank"><button className="mt-[5px] border p-1 border-black">View website</button></a>
           </div>
-          <div className="w-[350px] h-[320px] border border-black p-1 rounded-lg">
+          <div className="w-[350px] h-[fit-content] p-2 border border-black  rounded-lg max-sm:w-[95%]">
             <img src={image} className="" />
             <p className="mt-[5px] font-bold">ECOMMERCE</p>
             <p>Awell constructed ecommerce website using reactjs and typescript</p>
@@ -155,7 +155,7 @@ const Portfolio = () => {
         <div className="flex items-center justify-center p-5 flex-col gap-7">
           <p className="font-bold text-2xl">Let's work together on your next project</p>
           <p>Looking for a developer to help bring your project idea into reality? look no further because I've got you covered</p>
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-4 max-ssm:flex-col">
           <a href="https://www.instagram.com/inalajacob_16/" target="_blank"><FaInstagram size={50}/></a>
           <a href="https://github.com/Inala726" target="_blank"><FaGithub size={50}/></a>
           <a href="https://www.linkedin.com/in/inala-jacob-6b9664297/" target="_blank"><FaLinkedin size={50}/></a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'max-ssm': {'max': '260px'},
+        'max-sm': { 'max': '639px' },
+        'max-md': { 'max': '768px' },
+        'max-lg': { 'max': '1023px' },
+        'max-xl': { 'max': '1279px' },
+      },
+    },
   },
   plugins: [],
 }
